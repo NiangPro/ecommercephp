cb14d54ee97c1d22ebe65ea3ed4ededcd9c9a9a1
Fall codeur <fallmidadi1996@gmail.com>
2023-07-19 18:02:00 +0000

    accueil design shop

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site ecommerce</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="./assets/style.css">
+  </head>
 <body>  
-  <div class="bg-success">
     <nav class="navbar navbar-expand-lg navbar-light bg-success" style="border-bottom: 1px solid white;">
       <div class="row w-100 ml-auto">
         <div class="col-md-4">
@@ -45,9 +45,38 @@
         </ul>
       </div>
     </nav>
-  </div>
-  
+    <div class="container mt-2">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <div class="card-body card1">
+              <ul>
+                <li><a href="">Categories</a></li>
+                <li><a href="">Categories</a></li>
+                <li><a href="">Categories</a></li>
+                <li><a href="">Categories</a></li>
+                <li><a href="">Categories</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="card mb-3 card2">
+            <div class="card-header">
+              <span class="float-left text-bold title">HOT DEALS</span>
+              <span class="float-right text-info">man</span>
+            </div>
+            <div class="card-body">
+              <img class="d-flex justify-content-center m-auto" src="images/tshirt.png" alt="" width="100" height="200">
+            </div>
+            <div class="card-footer">
 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-8 img-accueil">
+
+        </div>
+      </div>
+    </div>
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
