6db56b73aaa267b697c169da4c14a3f99fab5b15
NiangPro <NiangProgrammeur@gmail.com>
2023-07-20 15:26:17 +0000

    refractory

---
+++ partials/navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-success" style="border-bottom: 1px solid white;">
+      <div class="row w-100 ml-auto">
+        <div class="col-md-4">
+          <a href="#" class="text-uppercase navbar-brand">SUNU<span style="color: red;">SHOP</span></a>
+        </div>
+        <div class="col-md-4">
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+        <div class="col-md-4">
+          <button class="btn btn-sm circle btn-light float-right rounded-pill">Pannier</button>
+        </div>
+      </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-4">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
