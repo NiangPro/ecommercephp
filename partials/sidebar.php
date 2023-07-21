de1ba2f6e9a5959aaa1bd333eb593f9ff641614d
Fall codeur <fallmidadi1996@gmail.com>
2023-07-21 13:58:15 +0000

    design index

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -25,6 +25,77 @@
         <img class="d-flex justify-content-center m-auto" src="images/cat-1.jpg" alt="" width="200" height="200">
     </div>
     <div class="card-footer">
-
+        <div class="d-flex w-100 justify-content-between">
+            <div class="float-left group" style="line-height: 1rem;">
+                <h5>Tshirt</h5>
+                <p>
+                    <i class="fa fa-star text-warning"></i> 
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                </p>
+                <p>5000 fr</p>
+            </div>
+            <div class="float-right" style="margin-top: 71px;">
+                <span>SIZE</span>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mb-3 card2">
+    <div class="card-header">
+        <strong class="float-left text-bold title">Les plus visités</strong>
+        <span class="float-right text-info">man</span>
+    </div>
+    <div class="card-body new-product">
+        <div class="d-flex w-100 justify-content-between mb-2">
+                <div class="float-left" style="line-height: 1rem;">
+                    <img class="img-fluid w-100" src="images/product-3.jpg" alt="">  
+                </div>
+                <div class="float-right group mt-4">
+                    <h5>Tshirt</h5>
+                    <p class="star">
+                        <i class="fa fa-star text-warning"></i> 
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <p class="text-danger">5000 fr</p>
+                </div>
+            </div>
+            <div class="d-flex w-100 justify-content-between mb-2">
+                <div class="float-left" style="line-height: 1rem;">
+                    <img class="img-fluid w-100" src="images/product-3.jpg" alt="">  
+                </div>
+                <div class="float-right group mt-4">
+                    <h5>Tshirt</h5>
+                    <p class="star">
+                        <i class="fa fa-star text-warning"></i> 
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <p class="text-danger">5000 fr</p>
+                </div>
+            </div>
+            <div class="d-flex w-100 justify-content-between mb-2">
+                <div class="float-left" style="line-height: 1rem;">
+                    <img class="img-fluid w-100" src="images/product-3.jpg" alt="">  
+                </div>
+                <div class="float-right group mt-4">
+                    <h5>Tshirt</h5>
+                    <p class="star">
+                        <i class="fa fa-star text-warning"></i> 
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                        <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <p class="text-danger">5000 fr</p>
+                </div>
+            </div>
     </div>
 </div>
--- views/home.view.php
+++ views/home.view.php
@@ -1,4 +1,4 @@
-<div class="container mt-3">
+<div class="container mt-5" style="margin-top: 130px!important;">
       <div class="row">
         <div class="col-md-4">
           <?php require_once("partials/sidebar.php"); ?>
@@ -48,6 +48,99 @@
                 </button>
               </div>
               <!-- fin slider -->
+              <!-- Collection -->
+              <div class="card card-product mt-3">
+                  <img src="images/accueil.jpg" class="d-block w-100" height="300" style="opacity: 0.5;"  >
+                  <div class="d-flex flex-column align-items-center justify-content-center collection mt-5 ml-5" >
+                      <div class="p-3 text-center" style="max-width: 700px;">
+                          <h3 class="text-info text-uppercase font-weight-bold mb-3">Nouvelle Collection</h4>
+                          <h5 class="text-danger mb-4">Tshirt Homme et Femme</h5>
+                          <a href="" class="btn btn-warning text-uppercase font-weight-bold text-black py-1 px-1">Voir panier</a>
+                      </div>
+                  </div>
+              </div>
+              <!-- fin collection -->
+              <div class="card mt-3">
+                <div class="card-header">
+                  <h3 class="text-bold">Sacs Homme et Femme</h3>
+                </div>
+                <div class="card-body">
+                  <div class="row px-xl-5 pb-3">
+                    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                        <div class="card product-item border-0 mb-4">
+                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img class="img-fluid w-100" src="images/product-1.jpg" alt="">
+                            </div>
+                            <div class="card-body border-left border-right text-center p-0 pt-2 pb-2 group">
+                                <h6 class="text-truncate mb-3">Robe Courte</h6>
+                                <p>
+                                    <i class="fa fa-star text-warning"></i> 
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                </p>
+                                <div class="d-flex justify-content-center">
+                                    <h6>20000 fcfa</h6>
+                                </div>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between bg-light border">
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-eye text-warning mr-1"></i></a>&nbsp;
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-shopping-cart text-warning mr-1"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                        <div class="card product-item border-0 mb-4">
+                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img class="img-fluid w-100" src="images/product-2.jpg" alt="">
+                            </div>
+                            <div class="card-body border-left border-right text-center p-0 pt-2 pb-2 group">
+                                <h6 class="text-truncate mb-3">Tshirt Enfant</h6>
+                                <p>
+                                    <i class="fa fa-star text-warning"></i> 
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                </p>
+                                <div class="d-flex justify-content-center">
+                                    <h6>15000 fcfa</h6><h6 class="text-muted ml-2"><del>20000 fcfa</del></h6>
+                                </div>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between bg-light border">
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-eye text-warning mr-1"></i></a>&nbsp;
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-shopping-cart text-warning mr-1"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                        <div class="card product-item border-0 mb-4">
+                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img class="img-fluid w-100" src="images/product-3.jpg" alt="">
+                            </div>
+                            <div class="card-body border-left border-right text-center p-0 pt-2 pb-2 group">
+                                <h6 class="text-truncate mb-3">TShirt Adulte</h6>
+                                <p>
+                                    <i class="fa fa-star text-warning"></i> 
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                </p>
+                                <div class="d-flex justify-content-center">
+                                    <h6>19000 fcfa</h6><h6 class="text-muted ml-2"><del>25000 fcfa</del></h6>
+                                </div>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between bg-light border">
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-eye text-warning mr-1"></i></a>&nbsp;
+                                <a href="" class="btn btn-outline-info btn-sm text-dark"><i class="fas fa-shopping-cart text-warning mr-1"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
       </div>
     </div>
